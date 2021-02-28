0fb11973bb72e88efd53e6f88f2a29abb1c595ca
Daniel Van der Vyver <576392@student.belgiumcampus.ac.za>
2021-02-28 15:25:36 +0200

    Changes

---
+++ BusinessLayer/Bookings.java
@@ -0,0 +1,24 @@
+package Project;
+
+public class Bookings {
+
+
+    public void makeBooking(){
+
+    }
+
+    public void bookingAvailable(){
+
+    }
+
+    public void checkBookings(){
+
+    }
+    
+    public void updateBooking(){
+
+    }
+
+    
+    
+}
