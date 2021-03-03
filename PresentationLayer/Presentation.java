43bc0b2e106bb3109115ad539742ab7519db0831
Wian Davel <wiandavel2000@gmail.com>
2021-03-03 22:33:30 +0200

    Booking details

--- PresentationLayer/Presentation.java
+++ PresentationLayer/Presentation.java
@@ -34,20 +34,32 @@ public class Presentation {
 
             if(regAnswer == "Y")
             {
-                System.out.print("Any bookings made? Y / N ");
-                String BookAnswer = scr.nextLine();
-            
-                if(BookAnswer == "Y"){
                 
-            }
             }
             else if(regAnswer == "N"){
                 cl.RegisterClient();
             }
                 break;
             //bla bla bla
+            //Weereens ek weet nie of dit onder case 2 of 1 kom nie. Volgens my(Wian) moet die eienaar kan sien wat die bookings al is. Ons moet net nog code hoe die bookings in n list in gaan en dan na n file toe
             case 2:
+            System.out.print("Any bookings made? Y / N ");
+                String BookAnswer = scr.nextLine();
             
+                if(BookAnswer == "Y"){
+                    List<Booking> Booking1 = new ArrayList<Booking>();
+
+                    System.out.println("--------------------------------------------------------------------");
+                    System.out.println("Type Event\tDate\tVenue Address\tTotalAdults\tTotalKids\tFood choice \tDecorations");
+                    System.out.println("--------------------------------------------------------------------");
+
+                    for (Booking item : Booking1) {
+                        System.out.println(item.type + "\t" + item.date + "\t\t" + item.venueAddress + "\t" + item.numadults + "\t" + item.numkids + "\t" + item.food + "\t" + item.deco);
+                    }
+
+                    System.out.println("--------------------------------------------------------------------");
+            
+            }
                 break;
             
             case 0:
