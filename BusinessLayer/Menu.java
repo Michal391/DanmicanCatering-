15bdac5e1f8b23516f2dfb0df35e2ed8c1064a41
Wian Davel <wiandavel2000@gmail.com>
2021-03-06 20:50:37 +0200

    commit

--- BusinessLayer/Menu.java
+++ BusinessLayer/Menu.java
@@ -88,9 +88,6 @@ public class Menu {
         
 
         while(menuNotFinished == true ){
-
-            System.out.print("\033[H\033[2J");  
-            System.out.flush();
             
             for(String menuitem : menuI){
                 menuarr = menuitem.split(",");
